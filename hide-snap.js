df09ed0ae89852f4b40f1f29c5b13c0d993bc639
Nathaniel <nathbrandal@gmail.com>
2021-10-21 14:52:46 +0200

    initial

---
+++ hide-snap.js
@@ -0,0 +1,9 @@
+function mod_hideSnap(){
+	if(document.querySelector(".snap-button [text-button]") !== null){
+		var el = document.querySelector(".snap-button [text-button]")
+		el.remove();
+	}
+	else{
+		console.log("no snap buttons to remove");
+	}
+}
