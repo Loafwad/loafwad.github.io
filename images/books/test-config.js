525b9e5b2a17dc25c693c4d05443b971b8c8bd9e
Loafwad <46791691+Loafwad@users.noreply.github.com>
2021-10-27 16:59:14 +0200

    Create test-config.js

---
+++ images/books/test-config.js
@@ -0,0 +1,5 @@
+var slideconfig = {};
+
+slideconfig.slides = [
+  "https://loafwad.github.io/images/books/test2/TestPDF2-1.jpg","https://loafwad.github.io/images/books/test2/TestPDF2-2.jpg", "https://loafwad.github.io/images/books/test2/TestPDF2-3.jpg", "https://loafwad.github.io/images/books/test2/TestPDF2-4.jpg"
+];
