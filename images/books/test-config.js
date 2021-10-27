01a564e06e008c905ea0d580096028bb5337e70b
Loafwad <46791691+Loafwad@users.noreply.github.com>
2021-10-27 17:01:41 +0200

    Update test-config.js

--- images/books/test-config.js
+++ images/books/test-config.js
@@ -1,5 +1,5 @@
-var slideconfig = {};
+var bookTest = {};
 
-slideconfig.slides = [
+bookTest.pages = [
   "https://loafwad.github.io/images/books/test2/TestPDF2-1.jpg","https://loafwad.github.io/images/books/test2/TestPDF2-2.jpg", "https://loafwad.github.io/images/books/test2/TestPDF2-3.jpg", "https://loafwad.github.io/images/books/test2/TestPDF2-4.jpg"
 ];
