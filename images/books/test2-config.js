7778137c1a7602d35ee8e1235694e9915754ea42
Loafwad <46791691+Loafwad@users.noreply.github.com>
2021-10-27 17:11:39 +0200

    Create test2-config.js

---
+++ images/books/test2-config.js
@@ -0,0 +1,5 @@
+var bookTest2 = {};
+
+bookTest2.pages = [
+  "https://loafwad.github.io/images/books/test/test_1.jpg","https://loafwad.github.io/images/books/test/test_3.jpg", "https://loafwad.github.io/images/books/test/test_3.jpg", "https://loafwad.github.io/images/books/test/test_4.jpg"
+];
